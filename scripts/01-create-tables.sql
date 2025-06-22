31f7f0d3c17c794c152686853411d7485e70a719
Chilaqui <comhector_@hotmail.com>
2025-06-22 04:38:34 -0600

    Modificando la tabla para Postgres

--- scripts/01-create-tables.sql
+++ scripts/01-create-tables.sql
@@ -1,7 +1,7 @@
 #Sistema de datos bancarios
 
 CREATE TABLE cliente (
-    id INT PRIMARY KEY AUTO:INCREMENT,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
