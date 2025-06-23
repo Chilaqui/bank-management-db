dde3b176364602df6af0f774c5b30b4e3750ee72
Hector <comhector_@hotmail.com>
2025-06-23 00:51:24 -0600

    Creacion de la tabla empleado

--- scripts/01-create-tables.sql
+++ scripts/01-create-tables.sql
@@ -25,3 +25,15 @@ CREATE TABLE cuenta(
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+CREATE TABLE empleado(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    puesto ENUM('Gerenete', 'Cajero', 'Asesor') NOT NULL,
+    departamento VARCHAR(50) NOT NULL,
+    fecha_ingreso DATE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telefono VARCHAR(15) NOT NULL
+)
